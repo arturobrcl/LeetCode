834bea93e953679037a332f8aace9965e9cee327
arturobrcl <52223777+arturobrcl@users.noreply.github.com>
2024-05-29 10:06:40 -0600

    Update ransom_note.py

--- Python/ransom_note.py
+++ Python/ransom_note.py
@@ -2,7 +2,19 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         ransomNote = list(ransomNote)
         magazine = list(magazine)
-        for i in range(0, len(ransomNote)):
-            if ransomNote[i] in magazine:
-                ransomNote[i].pop()
-                
+        len1 = len(ransomNote)
+        len2 = len(magazine)
+        i, j = 0, 0
+        while i < len1 and j < len2:
+            if ransomNote[i] == magazine[j]:
+                ransomNote[i] = ''
+                magazine[j] = ''
+                i += 1
+                j = 0
+            else:
+                j += 1
+        print(ransomNote)
+        if ransomNote == ['']*len1:
+            return True
+        else:
+            return False
