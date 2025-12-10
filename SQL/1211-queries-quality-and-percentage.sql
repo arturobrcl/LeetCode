818a878bea8d1a761747a5c2e4620ed7a8035fa4
arturobrcl <52223777+arturobrcl@users.noreply.github.com>
2025-12-09 20:17:19 -0600

    Create 1211-queries-quality-and-percentage.sql

---
+++ SQL/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT query_name, ROUND(SUM(rating/position) / COUNT(query_name), 2) AS quality, ROUND(COUNT(CASE WHEN rating < 3 THEN 1 END)*100 / COUNT(*), 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
